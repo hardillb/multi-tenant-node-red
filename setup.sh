dbf36738bbb4e5557bb877d8a411df5fe46c87e0
Ben Hardill <hardillb@gmail.com>
2020-09-21 18:30:45 +0100

    Fix typo

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir mongodb
-sudo chown -R 10001:65533 registery
+sudo chown -R 10001:65533 registry
 sudo chown -R 999:1000 mongodb
 docker build -t custom-node-red src/custom-node-red
 docker network create internal
