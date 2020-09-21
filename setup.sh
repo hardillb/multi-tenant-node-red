6c725551b005d702e1b37595e79d00ad6713858c
Ben Hardill <hardillb@gmail.com>
2020-09-21 18:31:42 +0100

    Another setup

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 mkdir mongodb
 sudo chown -R 10001:65533 registry
 sudo chown -R 999:1000 mongodb
-docker build -t custom-node-red src/custom-node-red
+docker build -t custom-node-red custom-node-red
 docker network create internal
