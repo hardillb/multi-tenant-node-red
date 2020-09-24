2836173af0322f0296ceee207487102d13f4591d
Dave Conway-Jones <conway@uk.ibm.com>
2020-09-24 13:29:09 +0100

    Merge branch 'master' of https://github.com/hardillb/multi-tenant-node-red

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 git submodules init
 git submodules update
+docker build -t custom-node-red custom-node-red
 mkdir mongodb
 sudo chown -R 10001:65533 registry
 sudo chown -R 999:1000 mongodb
