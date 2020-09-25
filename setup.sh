2fd27e7c2027eaa3d6b93baed797749b353c46c6
Dave Conway-Jones <conway@uk.ibm.com>
2020-09-25 17:23:45 +0100

    Fix setup so hostname substitution works

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ git submodule update
 mkdir -p mongodb
 sudo chown -R 10001:65533 registry
 sudo chown -R 999:1000 mongodb
-sed -i -e "s/docker-pi/$HOSTNAME/g" docker-compose.yml
-sed -i -e "s/docker-pi/$HOSTNAME/g" custom-node-red/settings.js
+hn=`hostname`
+sed -i -e "s/docker-pi/$hn/g" docker-compose.yml
+sed -i -e "s/docker-pi/$hn/g" custom-node-red/settings.js
 docker build -t custom-node-red custom-node-red
 docker network create internal
