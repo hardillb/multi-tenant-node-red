0f3fe8b2b97e85b39685ee9879f6c97cfd3ed9c7
Ben Hardill <hardillb@gmail.com>
2020-09-26 08:05:51 +0100

    Double tag the custom-node-red image

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ sudo chown -R 999:1000 mongodb
 hn=`hostname`
 sed -i -e "s/docker-pi/$hn/g" docker-compose.yml
 sed -i -e "s/docker-pi/$hn/g" custom-node-red/settings.js
-docker build -t custom-node-red custom-node-red
+docker build -t custom-node-red -t custom-node-red:`date +%Y%m%d%H%M%S` custom-node-red
 docker network create internal
